0ac9904e968923942a0060342675f0ccb84480ba
Dendi Mukti Putranto <dendimuktip@gmail.com>
2019-10-31 11:30:00 +0700

    modularisasi tahap 1

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ else:
 print("Dengan for")
 for i in range(0, 10):
     print(i+1, luas_segitiga)
+
+
+# modularisasi 1 => fungsi
+#
+# print("Segitiga 1")
+# alas = 20
+# tinggi = 3
+# luas_segitiga = alas * tinggi
+# print(luas_segitiga)
+
+def hitung_luas_segitiga(title, alas, tinggi):
+    print(title)
+    return (alas * tinggi) / 2
+
+print(hitung_luas_segitiga("Segitiga 1", 5, 15))
+print(hitung_luas_segitiga("Segitiga 2", 3, 6))
